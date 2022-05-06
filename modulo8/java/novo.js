3f0df67c4c38486346b93c04cfe175d83d7281b7
Paulo Henrique Siqueira <43135777+paulohscwb@users.noreply.github.com>
2022-05-06 18:16:33 -0300

    Update novo.js

--- modulo8/java/novo.js
+++ modulo8/java/novo.js
@@ -2,11 +2,15 @@
  * Specifies an envMap on an entity, without replacing any existing material
  * properties.
  */
+ 	  
 AFRAME.registerComponent('camera-cube-env', {
   schema: {
-	    resolution: { type:'number', default: 128},
-	    distance: {type:'number', default: 100000},
-	    interval: { type:'number', default: 1000},
+	    resolution: { type:'number', default: 512},
+	    distance: {type:'number', default: 10000},
+	    interval: { type:'number', default: 100},
+		matoverride: {type:'boolean', default: false},
+		metalness: { type:'float', default: 1.0},
+		roughness: { type:'float', default: 0.0},
 	    repeat: { type:'boolean', default: false}
 	  },
 
@@ -19,60 +23,74 @@ AFRAME.registerComponent('camera-cube-env', {
 	   * Called once when component is attached. Generally for initial setup.
 	   */
 	  init: function(){
-	    this.counter = this.data.interval;
-	    this.cam = new THREE.CubeCamera( 1.0, this.data.distance, this.data.resolution);
+		this.tick = AFRAME.utils.throttleTick(this.tick, this.data.interval, this);
+		
+		this.cam = new THREE.CubeCamera( 0.2, this.data.distance, this.data.resolution);
 		
 		this.cam.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
 		this.cam.renderTarget.texture.generateMipmaps = true;
 	    this.el.object3D.add( this.cam );
+		
+		//this method does target for skinned meshes and unskinned
+		this.loaded = this.el.sceneEl.addEventListener('model-loaded', () => {
+			
+			// Grab the mesh / scene.
+			const obj = this.el.getObject3D('mesh');
+			// Go over the submeshes and modify materials we want.
+			
+			obj.traverse(node => {
+				var myMesh = this.el.getObject3D('mesh');
+				
+				myMesh.visible = false;
 
-	    this.done = false;
-		var myCam = this.cam;
-		var myEl = this.el;
-		var myMesh = this.el.getObject3D('mesh');
+				AFRAME.scenes[0].renderer.autoClear = true;
+				var camVector = new THREE.Vector3();
+				this.el.object3D.getWorldPosition(camVector);
+				this.cam.position.copy(this.el.object3D.worldToLocal(camVector));
+				this.cam.update( AFRAME.scenes[0].renderer, this.el.sceneEl.object3D );
 
-		document.querySelector('a-scene').addEventListener('loaded', function (myCam, myEl, myMesh) {
-			if(myMesh){
-				myMesh.traverse( function( child ) { 
-					if ( child instanceof THREE.Mesh ) {
-						child.material.envMap = myCam.renderTarget.texture;
-						child.material.needsUpdate = true;
+				if (node.type.indexOf('Mesh') !== -1) {
+					if(this.data.matoverride == true){
+						console.log("override material = true");
+						node.material.metalness = this.data.metalness;
+						node.material.roughness = this.data.roughness;
 					}
-				});
-			}
-		});      
-			  
+					node.material.envMap = this.cam.renderTarget.texture;
+					node.material.needsUpdate = true;
+				}
+				myMesh.visible = true;
+			});
+		});
 	  },
 	  
 	  tick: function(t,dt){
-		var myCam = this.cam;
-	    if(!this.done){
-	      if( this.counter > 0){
-	        this.counter-=dt;
-	      }else{
-	        this.mesh = this.el.getObject3D('mesh');
-	        
-	        if(this.mesh){
-	            this.mesh.visible = false;
-	            AFRAME.scenes[0].renderer.autoClear = true;
-	            myCam.position.copy(this.el.object3D.worldToLocal(this.el.object3D.getWorldPosition()));
-	            myCam.update( AFRAME.scenes[0].renderer, this.el.sceneEl.object3D );
+			if(!this.done){
+				this.redraw(this.cam, this.el, this.el.getObject3D('mesh'));
+				if(!this.data.repeat){
+					this.done = true;
+				}
+			}
+	  },
+	  
+	  redraw: function (myCam, myEl, myMesh) {
+			if(this.el.getObject3D('mesh') != null && AFRAME.scenes[0] !=null){
+				myMesh.visible = false;
 
-	            this.mesh.traverse( function( child ) { 
-	                if ( child instanceof THREE.Mesh ){
-						child.material.envMap = myCam.renderTarget.texture;
-						child.material.needsUpdate = true;
-					}
-	            });
-	            this.mesh.visible = true;
-	        
-	            if(!this.data.repeat){
-	              this.done = true;
-	              this.counter = this.data.interval;
-	            }
-	        }
-	      }
-	    }
+				AFRAME.scenes[0].renderer.autoClear = true;
+				var camVector = new THREE.Vector3();
+				myEl.object3D.getWorldPosition(camVector);
+				myCam.position.copy(myEl.object3D.worldToLocal(camVector));
+				myCam.update( AFRAME.scenes[0].renderer, myEl.sceneEl.object3D );
+				if(myMesh){
+					myMesh.traverse( function( child ) { 
+						if ( child instanceof THREE.Mesh ) {
+							child.material.envMap = myCam.renderTarget.texture;
+							child.material.needsUpdate = true;
+						}
+					});
+				}
+				myMesh.visible = true;
+			}
 	  },
 
 	  /**
@@ -80,30 +98,18 @@ AFRAME.registerComponent('camera-cube-env', {
 	   * Generally modifies the entity based on the data.
 	   */
 	  update: function (oldData) {
-			this.counter = this.data.interval;
-				this.cam = new THREE.CubeCamera( 1.0, this.data.distance, this.data.resolution);
-			  this.cam.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
-	          this.el.object3D.add( this.cam );
-	          this.done = false;
-			  var myCam = this.cam;
-			  
-	          this.mesh = this.el.getObject3D('mesh');
-	          if(this.mesh){
-	            this.mesh.traverse( function( child ) { 
-	                if ( child instanceof THREE.Mesh ) {
-						child.material.envMap = myCam.renderTarget.texture;
-						myCam.renderTarget.texture.generateMipmaps = true;
-						child.material.needsUpdate = true;
-					}
-	            });
-	          }
+
+			this.redraw(this.cam, this.el, this.el.getObject3D('mesh'));
+
 	  },
 
 	  /**
 	   * Called when a component is removed (e.g., via removeAttribute).
 	   * Generally undoes all modifications to the entity.
 	   */
-	  remove: function () {},
+	  remove: function () {
+		  this.loaded.remove();
+	  },
 
 	  /**
 	   * Called on each scene tick.
